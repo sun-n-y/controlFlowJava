b7a408240357bd211b536f0a74336f29b56a6485
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-26 15:20:29 -0500

    add switch statement challenge

---
+++ src/SwitchChallenge.java
@@ -0,0 +1,28 @@
+public class SwitchChallenge {
+
+    public static void main(String[] args) {
+
+        char letter = 'Z';
+
+        switch (letter) {
+            case 'A':
+                System.out.println(letter + " = Able");
+                break;
+            case 'B':
+                System.out.println(letter + " = Baker");
+                break;
+            case 'C':
+                System.out.println(letter + " = Charlie");
+                break;
+            case 'D':
+                System.out.println(letter + " = Dog");
+                break;
+            case 'E':
+                System.out.println(letter + " = Easy");
+                break;
+            default:
+                System.out.println("Letter not found.");
+                break;
+        }
+    }
+}
